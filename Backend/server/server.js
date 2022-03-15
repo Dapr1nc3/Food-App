f47fe1e6a9c61da4a314b2e6687f33c579a7fd91
Brian Done <briguy2078@gmail.com>
2022-03-14 22:02:45 -0400

    added note to server.js file for public path

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("public")); // change this path to Frontend/public?
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
