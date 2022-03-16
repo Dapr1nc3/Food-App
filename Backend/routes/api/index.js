63a8c8fd8c5314be43e2c9f0fe045c245d444afc
Brian Done <briguy2078@gmail.com>
2022-03-16 14:58:34 -0400

    added user controller, user model, and user routes

--- Backend/routes/api/index.js
+++ Backend/routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const recipeRoutes = require('./recipe-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/recipes', recipeRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;
