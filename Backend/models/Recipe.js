32f2bc53236427814f7e95cc129fc416ef2cf277
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2022-03-16 18:16:52 -0400

    revise recipe schema and create typedefs for gql

--- Backend/models/Recipe.js
+++ Backend/models/Recipe.js
@@ -1,14 +1,31 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+// TODO: go over recipeSchema (might need to adjust the model to have an id, etc.)
 const RecipeSchema = new Schema(
   {
-    type: {
+    // Brian: 
+    // type: {
+    //   type: String,
+    //   unique: true,
+    //   required: true,
+    //   trim: true
+    // },
+    writtenByUsername: {
       type: String,
-      unique: true,
-      required: true,
-      trim: true
+      required: true
     },
+    recipeText: {
+      type: String,
+      required: "Please input your recipe",
+      minlength: [1, 'Character length must be between 1-280'],
+      maxlength: [1000, 'Can not exceed 1000 characters']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    }
   },
   {
     toJSON: {
