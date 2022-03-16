7c9d97b2bb70110ddf9022f8385d5128af90e00c
Brian Done <briguy2078@gmail.com>
2022-03-16 14:35:06 -0400

    added connection.js in config folder

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -7,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static('../Frontend/public'));
 
 app.use(require('./routes'));
 
@@ -21,4 +23,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/recipe-api', {
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`Server Connected Successfully on localhost:${PORT}`));
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`Server Successfully listening on localhost:${PORT}`));
+});
