6b4a1388939e6dc46b61f414b08f75c6f8a9dc4b
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-20 17:34:19 -0400

    made simple sign up form to test fetch calls for backend

--- Frontend/src/pages/SignUp.js
+++ Frontend/src/pages/SignUp.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Container, Form, Button } from "react-bootstrap";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <Container>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="" required="true" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" placeholder="" required="true" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="" required="true" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="" required="true" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
 };
 
 export default SignUp;
