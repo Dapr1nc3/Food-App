30726409f712a153ca43887c53406484973ac744
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-19 22:31:14 -0400

    added a log in page and form to the login page

--- Frontend/src/pages/logIn.js
+++ Frontend/src/pages/logIn.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Col, Container, Row, Form, Button } from "react-bootstrap";
 
 const SignIn = () => {
-  return <div>logIn</div>;
+  return (
+    <>
+      <Container>
+        <h1 className="shadow-sm text-info mt-5 p-3 text-center rounded">
+          Account Login
+        </h1>
+        <Row className="mt-5">
+          <Col
+            lg={5}
+            md={6}
+            sm={12}
+            className="p-5 m-auto shadow-sm rounded-lg"
+          >
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Remember me" />
+              </Form.Group>
+              <Button variant="info" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+        <h6 className="mt-5 p-5 text-center text-secondary">
+          Copyright &copy; to be determined. All Rights Reserved.
+        </h6>
+      </Container>
+    </>
+  );
 };
 
 export default SignIn;
