8441237118155b48c1e7461a8058ee601b903fb1
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-23 18:58:31 -0400

    Adding form submit for the sign up form

--- Frontend/src/pages/SignUp.js
+++ Frontend/src/pages/SignUp.js
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Post formData to API post(backend)
+  };
+
+  const handleForm = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  console.log(formData);
   return (
     <div>
       <Container>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" placeholder="" required="true" />
+            <Form.Control
+              onChange={handleForm}
+              type="text"
+              placeholder=""
+              name="username"
+              required="true"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="" required="true" />
+            <Form.Control
+              onChange={handleForm}
+              type="email"
+              placeholder=""
+              name="email"
+              required="true"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="" required="true" />
+            <Form.Control
+              onChange={handleForm}
+              type="password"
+              placeholder=""
+              name="password"
+              required="true"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
