7ae8821ffea798e2e7fdea48256c86ae1136f809
Brian Done <briguy2078@gmail.com>
2022-03-24 14:32:39 -0400

    updated index.js in frontend src

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,20 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-// the below is imported for testing purposes 
-import UsersTest from './components/UserTest'
+import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/* Users is a test file to verify connection of react to backend */}
-    <div><App /><UsersTest/></div>
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({
+  uri: 'https://48plr2roz4.see.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <div>
+      <h2>Food4Thought</h2>
+    </div>
+  );
+}
+
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
