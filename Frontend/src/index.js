0b95a9908685bd7b2465cb1af242cfa9747d3349
Brian Done <briguy2078@gmail.com>
2022-03-24 14:57:06 -0400

    updated index.js in frontend src, signup form now rendering

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import {
   ApolloClient,
   InMemoryCache,
@@ -13,17 +16,16 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-function App() {
-  return (
-    <div>
-      <h2>Food4Thought</h2>
-    </div>
-  );
-}
-
-render(
-  <ApolloProvider client={client}>
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root'),
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
