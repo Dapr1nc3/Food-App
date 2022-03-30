33d9df036dec04fc21e43f6504ff6ec8fa3501eb
dapr1nc3 <ddaprincem84@gmail.com>
2022-03-30 17:20:56 -0400

    fixing bugs

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
 const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -39,7 +40,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // FIXME: might need this to change if the public file cant be in the Frontend folder for the app to open
-app.use(express.static("../Frontend/build"));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/build/index.html"));
+});
 app.use(require("./routes"));
 // app.use(cors(corsOptions));
 
